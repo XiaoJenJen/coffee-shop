e32e006514239cea4d907c686d94c5ff440b2f76
Jenny <20jennyxu17@gmail.com>
2020-04-09 22:53:20 -0700

    update menu component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/global/backgroundSection"
 import Info from "../components/home/info"
+import Menu from "../components/home/menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -15,6 +16,7 @@ const IndexPage = ({ data }) => (
       styleClass="background-default"
     />
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -24,6 +26,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
